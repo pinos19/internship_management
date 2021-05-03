688cf8fef3a4ae24c9a85624a876d0ab7cb175a9
Nguyen Martin <martin_nguyen19@outlook.fr>
2021-05-03 15:37:09 +0200

    exit() superflu

--- admin/utilisateurs/ma_session.php
+++ admin/utilisateurs/ma_session.php
@@ -4,6 +4,6 @@
 	if(!isset($_SESSION['user'])) { // si l'utilisateur n'est pas conn�ct�
 		//si la variable $_SESSION['user'] n'existe pas			
 		header("location:../utilisateurs/login.php");
-		exit();
+		//exit();
 	}
 ?>
