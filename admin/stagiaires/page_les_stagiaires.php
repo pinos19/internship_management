1af9f025732af8b153b8202f9f499cc93cf2ab5e
Omar Dakkak <43930902+OmarDakkak@users.noreply.github.com>
2021-05-05 15:14:20 +0200

    Update page_les_stagiaires.php

--- admin/stagiaires/page_les_stagiaires.php
+++ admin/stagiaires/page_les_stagiaires.php
@@ -86,10 +86,10 @@
 	        <meta charset="utf-8" />
 	        <title> Les stagiaires </title>
 	        <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
-	        <link rel="stylesheet" type="text/css" href="../css/font-awesome.min.css">
-	        <link rel="stylesheet" type="text/css" href="../css/monStyle.css">
-	        <script src="../js/jquery-1.10.2.js"></>
-	        <script src="../js/bootstrap.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="../css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="../css/monStyle.css">
+		<script src="../js/jquery-1.10.2.js"></script>
+		<script src="../js/bootstrap.min.js"></script>
 
 	    </head>
 
