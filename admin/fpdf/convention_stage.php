d890537bf34e1eb3ab55738ef6375b3790b45c50
Omar Dakkak <43930902+OmarDakkak@users.noreply.github.com>
2021-05-05 15:34:44 +0200

    Add files via upload

--- admin/fpdf/convention_stage.php
+++ admin/fpdf/convention_stage.php
@@ -16,42 +16,72 @@ if (isset($_GET['as']))
 else
     $as = annee_scolaire_actuelle();
 
-$identite_stagiaire = $pdo->query("SELECT * FROM stagiaire WHERE id=$ids");
+$identite_stagiaire = $pdo->query("SELECT * FROM etudiant WHERE id_etudiant=$ids");
 $stagiaire = $identite_stagiaire->fetch();
-
-
-
 $nom_prenom = strtoupper($stagiaire['nom'] . "  " . $stagiaire['prenom']);
 
+$civilite = strtoupper($stagiaire['civilite']);
+$id_adresse = strtoupper($stagiaire['id_adresse']);
+$email = $stagiaire['email'];
+$tel = strtoupper($stagiaire['tel']);
+$annee_scolaire = strtoupper($stagiaire['annee_scolaire']);
+$id_campus = $stagiaire['id_campus'];
+$date_naissance = dateEnToDateFr($stagiaire['date_naissance']);
+
+// stage
+if ($annee_scolaire == 'ING1') {
+    $identite_stage = $pdo->query("SELECT * FROM stage WHERE id_etudiant = $ids and stage_niveau = 1");
+} else if ($annee_scolaire == 'ING2') {
+    $identite_stage = $pdo->query("SELECT * FROM stage WHERE id_etudiant = $ids and stage_niveau = 2");
+} else if ($annee_scolaire == 'ING3') {
+    $identite_stage = $pdo->query("SELECT * FROM stage WHERE id_etudiant = $ids and stage_niveau = 3");
+}
+
+$stage = $identite_stage->fetch();
+$type_stage = $stage['libelle'];
+$id_tuteur_interne = $stage['id_tuteur_interne'];
+$id_tuteur_externe = $stage['id_tuteur_externe'];
+
+// tuteur interne
+$identite_tuteur_interne = $pdo->query("SELECT * FROM tuteur WHERE id_tuteur = $id_tuteur_interne");
+$tuteur_interne = $identite_tuteur_interne->fetch();
+
+//tuteur externe
+$identite_tuteur_externe = $pdo->query("SELECT * FROM tuteur WHERE id_tuteur = $id_tuteur_externe");
+$tuteur_externe = $identite_tuteur_externe->fetch();
 
 
+$nom_tuteur_interne = $tuteur_interne['nom'] . " " . $tuteur_interne['prenom'];
+$nom_tuteur_externe = $tuteur_externe['nom'] . " " . $tuteur_externe['prenom'];
 
 
-
-$date_insc = dateEnToDateFr($stagiaire['date_inscription']);
-
-$num_insc = strtoupper($stagiaire['num_inscription']);
-
-$ent_accueil = strtoupper($stagiaire['entreprise_accueil']);
-
-$type_stage = strtoupper($stagiaire['type_de_stage']);
-$encadrant_interne = strtoupper($stagiaire['tuteur_interne']);
-$encadrant_externe = strtoupper($stagiaire['tuteur_externe']);
-
-$scolarite_stagiaire = $pdo->query("SELECT id_stagiaire,annee_scolaire,classe,nom as Nom_Filiere,niveau_diplome
-										FROM scolarite,filiere
-										WHERE filiere.id=scolarite.id_filiere
-										AND id_stagiaire=$ids
-										AND annee_scolaire='$as'");
-$scolarite = $scolarite_stagiaire->fetch();
-
-$filiere = strtoupper($scolarite['Nom_Filiere']);
-
-$niveau = strtoupper($scolarite['niveau_diplome']);
-
-$classe = strtoupper($scolarite['classe']);
+$id_entreprise_tuteur_externe = $tuteur_externe['id_entreprise'];
 
 
+//campus
+$identite_campus = $pdo->query("SELECT * FROM campus WHERE id_campus = $id_campus");
+$campus = $identite_campus->fetch();
+$nom_campus = strtoupper($campus['nom']);
+$filiere = $campus['filiere'];
+
+// entreprise accueil
+$identite_ent_accueil = $pdo->query("SELECT * FROM entreprise WHERE id_entreprise = $id_entreprise_tuteur_externe");
+$ent_accueil = $identite_ent_accueil->fetch();
+
+$nom_ent_accueil = strtoupper($ent_accueil['nom']);
+
+
+$id_adresse_ent_accueil = $ent_accueil['id_adresse'];
+
+//adresse ent accueil
+$identite_adresse_ent_accueil = $pdo->query("SELECT * FROM adresse WHERE id_adresse = $id_adresse_ent_accueil");
+$adresse_ent_accueil = $identite_adresse_ent_accueil->fetch();
+
+
+$indicatif_adresse_ent_accueil = strtoupper($adresse_ent_accueil['indicatif']);
+$rue_adresse_ent_accueil = strtoupper($adresse_ent_accueil['rue']);
+$ville_adresse_ent_accueil = strtoupper($adresse_ent_accueil['ville']);
+$code_postal_adresse_ent_accueil = strtoupper($adresse_ent_accueil['code_postal']);
 
 
 require('fpdf.php');
@@ -73,8 +103,8 @@ $pdf->Ln(18);
 $pdf->SetFont('Arial', 'B', 16);
 
 // Titre
-$pdf->Cell(0, 10, 'CONVENTION DE STAGE', 'TB', 1, 'C');
-$pdf->Cell(0, 10, 'N°:' . $num_insc, 0, 1, 'C');
+$pdf->Cell(0, 10, "Fiche d'informations", 'TB', 1, 'C');
+$pdf->Cell(0, 10, 'N°:' . $ids, 0, 1, 'C');
 
 // Saut de ligne
 $pdf->Ln(5);
@@ -97,23 +127,21 @@ $pdf->Write($h, $nom_prenom . "\n");
 $pdf->SetFont('', '');
 
 
-$pdf->Write($h, $retrait . "Inscrit (e) le : " . $date_insc . " Sous le N° : " . $num_insc . " \n");
-
 $pdf->Write($h, $retrait . "Filière :  " . $filiere . " \n");
 
-$pdf->Write($h, $retrait . "Niveau :  " . $classe . " \n");
+$pdf->Write($h, $retrait . "Niveau :  " . $annee_scolaire . " \n");
 
 $pdf->Write($h, $retrait . "Année de formation :  " . $as . "  \n");
 
-$pdf->Write($h, "Poursuit ses étude en  " . $classe . " à l'EILCO  et cela pour l'année scolaire en cours  " . $as . ".  \n");
+$pdf->Write($h, "Poursuit ses étude en  " . $annee_scolaire . " à l'EILCO  et cela pour l'année scolaire en cours  " . $as . ".  \n");
 
 $pdf->Write($h, $retrait . "Effectue son stage " . $type_stage . " à l'entreprise/établissement :");
 $pdf->SetFont('', 'BIU');
-$pdf->Write($h, $ent_accueil);
+$pdf->Write($h, $nom_ent_accueil);
 $pdf->SetFont('', '');
 $pdf->Write($h, ".\n");
-$pdf->Write($h, $retrait . "Son encadrant pédagogique interne à l'EILCO est : " . $encadrant_interne . ".\n");
-$pdf->Write($h, $retrait . "Son tuteur à l'établissement d'accueil est : " . $encadrant_externe . ".\n");
+$pdf->Write($h, $retrait . "Son encadrant pédagogique interne à l'EILCO est : " . $nom_tuteur_interne . ".\n");
+$pdf->Write($h, $retrait . "Son tuteur à l'établissement d'accueil est : " . $nom_tuteur_externe . ".\n");
 
 
 $pdf->Write($h, "La présente attestation est délivrée à l'intéressé Pour servir et valoir ce que de droit. \n\n");
